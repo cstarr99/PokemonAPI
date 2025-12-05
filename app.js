65c95fb1caf060555410404efaae325295dd76bd
Chase Starr <chasestarr9@gmail.com>
2025-12-05 12:11:47 -0500

    added all api calls

--- app.js
+++ app.js
@@ -1,26 +1,71 @@
 const searchBtn = document.querySelector(".search-btn");
 const searchInput = document.querySelector("#search-input");
+const pokemonName = document.querySelector(".pokemon-name");
+const pokemonType = document.querySelector(".pokemon-type");
+const pokemonPicture = document.querySelector(".pokemon-picture");
+const bodyElement = document.body;
 
 searchBtn.addEventListener("click", async (e) => {
   e.preventDefault();
+  clearData();
   const userInput = searchInput.value;
-  changeColor(userInput);
-  const pokemonData = await getPokemonData(userInput);
+  const rawPokemonData = await getPokemonAPI(userInput);
+  changeColor(userInput, rawPokemonData);
+  getPokemonName(rawPokemonData);
+  getPokemonType(rawPokemonData);
+  getPokemonPicture(rawPokemonData);
 });
 
-async function getPokemonData(name) {
+async function getPokemonAPI(name) {
   try {
     const currentPokemonURL = `https://pokeapi.co/api/v2/pokemon/${name}/`;
     const pokemonData = await fetch(currentPokemonURL);
     if (!pokemonData.ok) {
       console.log("Fetch could not be completed");
+    } else {
+      return await pokemonData.json();
     }
-    return await pokemonData.json();
   } catch (e) {
     console.error("Failed: ", e);
   }
 }
 
-async function changeColor(name) {
-  //change background color of page based on pokemon color
+async function changeColor(name, rawPokemonData) {
+  //getting pokemon's color from original rawPokemonData
+  try {
+    const speciesURL = rawPokemonData.species.url;
+    const speciesData = await fetch(speciesURL);
+    const rawSpeciesData = await speciesData.json();
+    const pokemonColor = rawSpeciesData.color.name;
+    bodyElement.style.backgroundColor = pokemonColor;
+  } catch (e) {
+    console.error("Failed: ", e);
+  }
 }
+
+async function getPokemonName(rawPokemonData) {
+  const div = document.createElement("div");
+  div.innerText = rawPokemonData.name;
+  pokemonName.append(div);
+  div.classList.add("pokemon-added-data");
+}
+
+async function getPokemonType(rawPokemonData) {
+  const div = document.createElement("div");
+  div.innerText = rawPokemonData.types[0].type.name;
+  pokemonType.append(div);
+  div.classList.add("pokemon-added-data");
+}
+
+async function getPokemonPicture(rawPokemonData) {
+  const img = document.createElement("img");
+  img.src = rawPokemonData.sprites.front_default;
+  pokemonPicture.append(img);
+  img.classList.add("pokemon-added-data");
+}
+
+function clearData() {
+  //clear previous data
+}
+
+function listErrors() {}
